d0a8e4dc68c503ec583b26dd28d36750455cdf18
Vandana Nayak <nvandana1@gmail.com>
2023-06-25 11:02:34 +0530

    navbar

--- recipe/src/app/app.module.ts
+++ recipe/src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -8,13 +12,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { ShoppingComponent } from './shopping/shopping.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipeComponent,
-    ShoppingComponent,
+    ShoppingComponent,RecipeListComponent,RecipeDetailsComponent,RecipeItemComponent,ShoppingComponent,ShoppingEditComponent
     
   ],
   imports: [
@@ -22,6 +27,7 @@ import { ShoppingComponent } from './shopping/shopping.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
